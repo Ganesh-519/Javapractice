e0f22fdbed69ee5ec5e4b9ba03a5d5a44cad3f0c
txl020301@darden.com <txl020301@darden.com>
2023-11-20 19:39:31 +0530

    commited

---
+++ javaopps/javaopps/src/Strings/StringBuilderDelete.java
@@ -0,0 +1,9 @@
+package Strings;
+
+public class StringBuilderDelete {
+	public static void main(String args[]){  
+		StringBuilder sb=new StringBuilder("Ganga");  
+		sb.delete(1,3);  
+		System.out.println(sb);  
+		}  
+}
