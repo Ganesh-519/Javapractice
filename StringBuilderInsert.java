6e2a7f4ad0d2b76377118e975e39e02a14ca6d07
Ganesh-519 <96115979+Ganesh-519@users.noreply.github.com>
2023-11-20 21:05:27 +0530

    Delete StringBuilderInsert.java

--- StringBuilderInsert.java
+++
@@ -1,9 +0,0 @@
-package Strings;
-
-public class StringBuilderInsert {
-	public static void main(String args[]){  
-		StringBuilder sb=new StringBuilder("Hello ");  
-		sb.insert(1,"world"); 
-		System.out.println(sb); 
-		}  
-}
