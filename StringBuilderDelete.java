2d2bea3cb49965ef34af748f4d2362c123c4ed0c
Ganesh-519 <96115979+Ganesh-519@users.noreply.github.com>
2023-11-20 21:04:25 +0530

    Delete StringBuilderDelete.java

--- StringBuilderDelete.java
+++
@@ -1,9 +0,0 @@
-package Strings;
-
-public class StringBuilderDelete {
-	public static void main(String args[]){  
-		StringBuilder sb=new StringBuilder("Ganga");  
-		sb.delete(1,3);  
-		System.out.println(sb);  
-		}  
-}
